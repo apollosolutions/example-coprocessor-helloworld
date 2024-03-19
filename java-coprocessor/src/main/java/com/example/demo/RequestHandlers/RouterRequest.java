4ed96f923f22ad77b275b954efc45683358425fe
FineAndDanD <daniel.dorsey@apollographql.com>
2024-03-19 16:36:01 -0400

    Added a js README and updated my java example

--- java-coprocessor/src/main/java/com/example/demo/RequestHandlers/RouterRequest.java
+++ java-coprocessor/src/main/java/com/example/demo/RequestHandlers/RouterRequest.java
@@ -8,11 +8,20 @@ import java.util.UUID;
 
 public class RouterRequest {
     public RouterRequestBody handle(RouterRequestBody request) {
-        // In this Java sample, we are mapping the request sent by the router to a model called "RouterRequestBody"
-        // Please review that class in the "Models" folder to review what you're able to retrieve at this stage
+        // This is the object sent by the Router that you can act upon to update headers, context, auth claims, etc
         // If you update the "control" property from "Continue" to something like { "break": 400 }, it will terminate the request and return the specified HTTP error
         // See: https://www.apollographql.com/docs/router/customizations/coprocessor/
+        // The object sent by the Router has been mapped to a `RouterRequestBody` object by the `@RequestBody` annotation in the `CoprocessorController` class
+        System.out.println(request.getHeaders());
+        System.out.println(request.getMethod());
+        System.out.println(request.getContext().getEntries());
+        System.out.println(request.getSDL());
+        System.out.println(request.getVersion());
+        System.out.println(request.getBody());
+        System.out.println(request.getStage());
+        System.out.println(request.getPath());
         System.out.println(request.getControl());
+        System.out.println(request.getId());
 
         return request;
     }
