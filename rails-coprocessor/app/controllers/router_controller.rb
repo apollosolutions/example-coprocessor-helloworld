814b56f95b7d74fcb0d0d7c589b774663d5358b3
FineAndDanD <daniel.dorsey@apollographql.com>
2024-05-22 16:50:27 -0400

    Refactor the router controller code

--- rails-coprocessor/app/controllers/router_controller.rb
+++ rails-coprocessor/app/controllers/router_controller.rb
@@ -2,14 +2,15 @@ require_relative "./../handlers/router_request_handler"
 
 class RouterController < ApplicationController
     def stage
+        response = '{}'
         case params['stage']
         when 'RouterRequest'
-            RouterRequestHandler.handle(request, logger)
+            response = RouterRequestHandler.handle(request, logger)
         else
             logger.error 'No valid stage provided'
         end
 
-        render :json => request.raw_post
+        render :json => response
     end
     def index
         @message = "Please visit the Apollo docs to learn more about coprocessing: https://www.apollographql.com/docs/router/customizations/coprocessor/"
--- rails-coprocessor/app/handlers/router_request_handler.rb
+++ rails-coprocessor/app/handlers/router_request_handler.rb
@@ -4,6 +4,8 @@ class RouterRequestHandler
         # If you update the "control" property from "Continue" to something like { "break": 400 }, it will terminate the request and return the specified HTTP error
         # See: https://www.apollographql.com/docs/router/customizations/coprocessor/
         logger.debug request.raw_post
+
+        request.raw_post
     end
 end
         
